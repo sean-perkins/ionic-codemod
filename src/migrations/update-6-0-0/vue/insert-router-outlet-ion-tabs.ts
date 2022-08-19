75426f7e2bbf43c99ed015960f3a2585d37bf1c5
Sean Perkins <sean@ionic.io>
2022-08-19 14:11:01 -0400

    refactor(migration): use constant selectors

--- src/migrations/update-6-0-0/vue/insert-router-outlet-ion-tabs.ts
+++ src/migrations/update-6-0-0/vue/insert-router-outlet-ion-tabs.ts
@@ -1,6 +1,10 @@
 import { SchematicContext, Tree } from "@angular-devkit/schematics";
 import { parse } from "node-html-parser";
 
+
+const ION_ROUTER_OUTLET_SELECTOR = 'ion-router-outlet';
+const ION_TABS_SELECTOR = 'ion-tabs';
+
 /**
  * Inserts <ion-router-outlet> inside <ion-tabs> when it is missing.
  */
@@ -8,19 +12,18 @@ export const insertRouterOutletIonTabs = (tree: Tree, context: SchematicContext)
   tree.getDir('src').visit(async filePath => {
     if (filePath.endsWith('.vue')) {
       const fileContent = tree.readText(filePath);
-
       // Converts the Vue file contents into an AST-like DOM structure
       const root = parse(fileContent);
 
-      const tabs = root.querySelectorAll('ion-tabs');
+      const tabs = root.querySelectorAll(ION_TABS_SELECTOR);
 
       if (tabs.length > 0) {
         tabs.forEach(tab => {
-          const routerOutlet = tab.querySelector('ion-router-outlet');
+          const routerOutlet = tab.querySelector(ION_ROUTER_OUTLET_SELECTOR);
           if (!routerOutlet) {
-            context.logger.info(`Adding ion-router-outlet to <ion-tabs> in ${filePath}`);
-
-            tab.insertAdjacentHTML('afterbegin', '\n<ion-router-outlet></ion-router-outlet>\n');
+            context.logger.info(`Adding ${ION_ROUTER_OUTLET_SELECTOR} to <${ION_TABS_SELECTOR}> in ${filePath}`);
+            // Inserts <ion-router-outlet> inside <ion-tabs> at the first position
+            tab.insertAdjacentHTML('afterbegin', `\n<${ION_ROUTER_OUTLET_SELECTOR}></${ION_ROUTER_OUTLET_SELECTOR}>\n`);
 
             const output = root.toString();
 
